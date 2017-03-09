bbf9559ce310ce5c6fca5eb42d55697679d83f3c
Scott Wu <scott.wu.hjx@gmail.com>
2017-03-09 16:53:34 -0500

    Converted library to header only

--- Utilities/TCPSocketClient.h
+++ Utilities/TCPSocketClient.h
@@ -55,19 +55,46 @@ public:
 	* @param address	The address to bind to
 	* @param port		The port to listen on
 	*/
-	TCPSocketClient(std::string inAddress, int inPort);
+	TCPSocketClient(std::string inAddress, int inPort) :
+		address(inAddress), port(inPort), connected(0) {
+		if ((socketId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+			printf("TCPSocketClient: Could not create socket.\n");
+			return;
+		}
+
+		addr.sin_family = AF_INET;
+		// TODO Support IPv6, use InetPton (inet_addr is deprecated)
+		addr.sin_addr.s_addr = inet_addr(address.c_str());
+		addr.sin_port = htons(port);
+
+		if (connect(socketId, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			printf("TCPSocketClient: Error connecting to the socket address.\n");
+			return;
+		}
+
+		connected = true;
+	};
 
 	/**
 	* Deconstructor
 	*/
-	~TCPSocketClient();
+	~TCPSocketClient() {
+		if (connected) {
+			closesocket(socketId);
+		}
+
+		connected = false;
+	};
 
 	/**
 	* Whether or not the client is connected to the server.
 	*
 	* @return		Boolean whether or not the client is connected to the server
 	*/
-	int isConnected();
+	int isConnected() {
+		return connected;
+	};
 
 	/**
 	* Read bytes of data from the port.
@@ -77,7 +104,13 @@ public:
 	* @param maxlen	The maximum number of bytes to read
 	* @return		The number of bytes read
 	*/
-	int read(char * outBuffer, unsigned int maxlen);
+	int read(char * outBuffer, unsigned int buffer_len) {
+		unsigned int len;
+
+		len = recv(socketId, outBuffer, buffer_len, NULL);
+
+		return len;
+	};
 
 	/**
 	* Write bytes of data to the port.
@@ -86,10 +119,23 @@ public:
 	* @param len		The number of bytes to write
 	* @return		Whether or not the write was successful
 	*/
-	int write(const char * buffer, unsigned int len);
+	int write(const char * buffer, unsigned int len) {
+		if (send(socketId, buffer, len, NULL) < 0) {
+			return 0;
+		}
+		// TODO Handle error codes and reconnection
+
+		return 1;
+	};
 
 	/**
 	* Close the connection.
 	*/
-	void close();
+	void close() {
+		if (connected) {
+			closesocket(socketId);
+		}
+
+		connected = false;
+	};
 };
