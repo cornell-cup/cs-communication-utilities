ddab55712306654c8b83c957262508101b278049
Scott Wu <scott.wu.hjx@gmail.com>
2017-09-10 18:55:05 -0400

    Add simple send/receive test

--- build.py
+++ build.py
@@ -6,39 +6,52 @@ import platform
 CC = "g++"
 
 OPTS = [
-  "-std=c++11",
-  "-O2",
-  "-Wall",
-  "-pthread",
+    "-std=c++11",
+    "-O2",
+    "-Wall",
+    "-pthread",
 ]
 
 HEADER_INCLUDES = [
-  "-I", "Utilities",
-  "-I", "lib/googletest/include",
+    "-I", "Utilities",
+]
+TEST_HEADER_INCLUDES = [
+    "-I", "lib/googletest/include",
+]
+
+TEST_STATIC_LIBRARIES = [
+    "-L", "lib/googletest/lib",
+    "-l", "gtest",
 ]
 
 if platform.system() == "Windows" :
-  SHARED_LIBRARIES = [
-    "lib/googletest/lib/libgtest.dll",
-  ]
+    SHARED_LIBRARIES = [
+        "lib/googletest/lib/libgtest.dll",
+    ]
 elif platform.system() == "Linux":
-  SHARED_LIBRARIES = [
-    "lib/googletest/lib/libgtest.a",
-  ]
+    SHARED_LIBRARIES = [
+        "lib/googletest/lib/libgtest.a",
+    ]
 else:
-  SHARED_LIBRARIES = [] #some other platform
+    SHARED_LIBRARIES = [] #some other platform
 
 OUTPUT_DIR = "bin"
 if not os.path.isdir(OUTPUT_DIR):
-  os.mkdir(OUTPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
 
 OUTPUT = os.path.join(OUTPUT_DIR, "Utilities.exe")
+TEST_OUTPUT = os.path.join(OUTPUT_DIR, "UtilitiesTest.exe")
 
 SRC = ["Utilities/Utilities.cpp"]
+TEST_SRC = ["test/UtilitiesTest.cpp"]
 
 args = [CC] + OPTS + HEADER_INCLUDES + SRC + ["-o", OUTPUT]
 print("Calling", args)
 subprocess.call(args)
 
+args = [CC] + OPTS + TEST_HEADER_INCLUDES + HEADER_INCLUDES + TEST_SRC + ["-o", TEST_OUTPUT] + TEST_STATIC_LIBRARIES
+print("Calling", args)
+subprocess.call(args)
+
 for s in SHARED_LIBRARIES:
-  shutil.copyfile(s, os.path.join(OUTPUT_DIR, os.path.basename(s)))
+    shutil.copyfile(s, os.path.join(OUTPUT_DIR, os.path.basename(s)))
---
+++ test/UDPSocketTest.h
@@ -0,0 +1,37 @@
+#include "UDPSocketClient.h"
+#include "UDPSocketServer.h"
+
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <thread>
+
+class UDPSocketTest : public testing::Test {
+protected:
+    int port;
+    UDPSocketServer server;
+    UDPSocketClient client;
+
+    UDPSocketTest() : port((std::rand() % 10000) + 50000), server("127.0.0.1", port), client("127.0.0.1", port) {}
+
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+        server.close();
+        client.close();
+    }
+};
+
+TEST_F(UDPSocketTest, SendReceive) {
+    char data[5] = "test";
+    char * result = new char[4096];
+    server.server([result](char * buffer, unsigned int buffer_len) {
+        std::strncpy(result, buffer, buffer_len);
+    });
+    client.write(data, 5);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT_STREQ(data, result);
+    delete result;
+}
