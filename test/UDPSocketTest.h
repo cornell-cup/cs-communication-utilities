7306091b72b8850a509c4f08ba04939daf3d11a6
Scott Wu <scott.wu.hjx@gmail.com>
2017-09-11 13:22:24 -0400

    Add second test for UDP sockets

--- test/UDPSocketTest.h
+++ test/UDPSocketTest.h
@@ -29,9 +29,27 @@ TEST_F(UDPSocketTest, SendReceive) {
     server.server([result](char * buffer, unsigned int buffer_len) {
         std::strncpy(result, buffer, buffer_len);
     });
-    client.write(data, 5);
 
+    client.write(data, 5);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     EXPECT_STREQ(data, result);
+    delete result;
+}
+
+TEST_F(UDPSocketTest, SendReceive2) {
+    char data1[6] = "test1";
+    char data2[6] = "test2";
+    char * result = new char[4096];
+    server.server([result](char * buffer, unsigned int buffer_len) {
+        std::strncpy(result, buffer, buffer_len);
+    });
+
+    client.write(data1, 6);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT_STREQ(data1, result);
+
+    client.write(data2, 6);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT_STREQ(data2, result);
     delete result;
 }
